0294791c9f7768b1697a1a14e7bc6d385019c29a
Sharmina Akhtar <sakht318@gmail.com>
2018-08-09 11:38:20 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector(){
+  $('p')
+}
+
+function lastImageSelector(){
+  $('div img:last-child')
+}
+
+function ninjaBabySelector(){
+  $('#baby-ninja')
+}
+
+function divSelector(){
+  $(' div.pics')
+}
+
+function firstListItem(){
+  $('ul li:first-child')
+}
